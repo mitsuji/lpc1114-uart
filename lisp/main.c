b1bc6fe22d9836e36d41bd17aa7495532e6bb5e3
mitsuji <mitsuji@na-s.jp>
2017-08-25 12:25:18 +0900

    ok

--- lisp/main.c
+++ lisp/main.c
@@ -21,11 +21,10 @@ void test_allocstr_print(uint16_t * strs, int strs_len);
 void test_allocstr_free(uint16_t * strs, int strs_len, uint16_t str);
 void test_allocstr()
 {
-  char * head = malloc(2048);
-  allocstr_init(head,2048);
-
+  char head[100];
   uint16_t strs[5];
-  uint16_t sf;
+
+  allocstr_init(head,sizeof(head));
   
   allocstr_alloc(&strs[0], "Alpha");
   allocstr_alloc(&strs[1], "Bravo");
@@ -40,12 +39,30 @@ void test_allocstr()
   allocstr_alloc(&strs[1], "November");
   test_allocstr_print(strs, (sizeof(strs)/sizeof(uint16_t)));
   
-  test_allocstr_free(strs,(sizeof(strs)/sizeof(uint16_t)), strs[1]);
+  test_allocstr_free(strs,(sizeof(strs)/sizeof(uint16_t)), strs[3]);
   test_allocstr_print(strs, (sizeof(strs)/sizeof(uint16_t)));
 
   allocstr_alloc(&strs[3], "Uniform");
   test_allocstr_print(strs, (sizeof(strs)/sizeof(uint16_t)));
+
+  test_allocstr_free(strs,(sizeof(strs)/sizeof(uint16_t)), strs[1]);
+  test_allocstr_free(strs,(sizeof(strs)/sizeof(uint16_t)), strs[2]);
+  test_allocstr_free(strs,(sizeof(strs)/sizeof(uint16_t)), strs[3]);
+  test_allocstr_print(strs, (sizeof(strs)/sizeof(uint16_t)));
+
+  test_allocstr_free(strs,(sizeof(strs)/sizeof(uint16_t)), strs[0]);
+  test_allocstr_free(strs,(sizeof(strs)/sizeof(uint16_t)), strs[4]);
+  test_allocstr_print(strs, (sizeof(strs)/sizeof(uint16_t)));
   
+  allocstr_alloc(&strs[1], "B");
+  test_allocstr_print(strs, (sizeof(strs)/sizeof(uint16_t)));
+  
+  allocstr_alloc(&strs[0], "A");
+  allocstr_alloc(&strs[2], "C");
+  allocstr_alloc(&strs[3], "D");
+  allocstr_alloc(&strs[4], "E");
+  test_allocstr_print(strs, (sizeof(strs)/sizeof(uint16_t)));
+
 }
 
 void test_allocstr_print(uint16_t * strs, int strs_len)
